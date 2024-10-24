6f0aa2a5ff0fe12415021ab61180e0fd16cfaa3e
fami0110 <masandofami@gmail.com>
2024-10-24 22:29:46 +0700

    some tweaks

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # Google-Maps-Scrapper
-Google Maps data scrapper using selenium python
 
-`Usage: ./scrapper.exe [-L FILE] [-a IMG_AMOUNT]`
-* Download and run the compiled file "dist/scrapper.exe"
-* The example for FILE is "links.txt"
+Google Maps data scrapper using Selenium in Python. This tool allows you to extract information about various locations from Google Maps, including names, addresses, contact, schedules, and more.
+
+## Features
+
+- Scrapes essential data from Google Maps listings.
+- Outputs data in a structured JSON format.
+- Can scrap and download images from Google maps.
+- There are 2 modes available, **dictionary mode** and **singgle mode**.
+- The amount download of the image can be ajust.
+
+## How To Use
+
+1. Download and run the compiled file "dist/scrapper.exe"
+2. Example usage: `./scrapper.exe [-L FILE] [-a IMG_AMOUNT]`
+3. If you want singgle mode, just run `./scrapper.exe`
+4. The example for [FILE] is "links.txt"
--- scrapper.py
+++ scrapper.py
@@ -9,12 +9,48 @@ from time import sleep
 import json
 import re
 from tqdm import tqdm
+from colorama import Fore as c, init as c_init
 
-def querySelector(rel, selector):
-    return rel.find_element(By.CSS_SELECTOR, selector)
+def querySelector(rel, selector, wait=True, timeout=5):
+    if wait:
+        res = WebDriverWait(rel, timeout).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, selector))
+        )
+    else:
+        res = rel.find_element(By.CSS_SELECTOR, selector)
+    return res
 
-def querySelectorAll(rel, selector):
-    return rel.find_elements(By.CSS_SELECTOR, selector)
+def querySelectorAll(rel, selector, wait=True, timeout=5):
+    if wait:
+        res = WebDriverWait(rel, timeout).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, selector))
+        )
+    else:
+        res = rel.find_elements(By.CSS_SELECTOR, selector)
+    return res
+
+day_generalization = {
+    "Senin": "mon",
+    "Monday": "mon",
+
+    "Selasa": "tue",
+    "Tuesday": "tue",
+
+    "Rabu": "wed",
+    "Wednesday": "wed",
+
+    "Kamis": "thu",
+    "Thursday": "thu",
+
+    "Jumat": "fri",
+    "Friday": "fri",
+
+    "Sabtu": "sat",
+    "Saturday": "sat",
+
+    "Minggu": "sun",
+    "Sunday": "sun",
+}
 
 
 def scrap(url, img_amount = 0):
@@ -33,55 +69,51 @@ def scrap(url, img_amount = 0):
     schedules = {}
     images = []
 
-    print("\nSTATUS: Get Place Name...")
+    print(f"{c.YELLOW}\nSTATUS: Get Place Name...{c.RESET}")
     try:
-        e_name = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "h1.DUwDvf.lfPIob"))
-        )
+        e_name = querySelector(driver, "h1.DUwDvf.lfPIob")
         name = e_name.get_attribute('innerText').strip()
 
-        print("FOUND:", name)
+        print(f"{c.GREEN}FOUND:", name, c.RESET)
     except:
-        print('Failed to Get Name!')
+        print(f'{c.RED}Failed to Get Name!{c.RESET}')
         
 
-    print("\nSTATUS: Get Place Rate...")
+    print(f"{c.YELLOW}\nSTATUS: Get Place Rate...{c.RESET}")
     try:
         e_rate = querySelector(driver, 'div.F7nice span[aria-hidden="true"]')
         rate = float(e_rate.get_attribute('innerText').strip().replace(',', '.'))
 
-        print("FOUND:", rate)
+        print(f"{c.GREEN}FOUND:", rate, c.RESET)
     except:
-        print('Failed to Get Rate!')
+        print(f'{c.RED}Failed to Get Rate!{c.RESET}')
 
 
-    print("\nSTATUS: Get Place Address...")
+    print(f"{c.YELLOW}\nSTATUS: Get Place Address...{c.RESET}")
     try:
         e_address = querySelector(driver, 'button.CsEnBe[data-item-id="address"]')
         address = e_address.get_attribute('aria-label').strip()
 
-        print("FOUND:", address)
+        print(f"{c.GREEN}FOUND:", address, c.RESET)
     except:
-        print('Failed to Get Address!')
+        print(f'{c.RED}Failed to Get Address!{c.RESET}')
 
     
-    print("\nSTATUS: Get Place Contact...")
+    print(f"{c.YELLOW}\nSTATUS: Get Place Contact...{c.RESET}")
     try:
         span = querySelector(driver, 'span.google-symbols.NhBTye.PHazN')
         parent = span.find_element(By.XPATH, "./ancestor::div[@class='AeaXub']")
         e_contact = querySelector(parent, 'div.Io6YTe')
         contact = e_contact.get_attribute('innerText')
 
-        print("FOUND:", contact)
+        print(f"{c.GREEN}FOUND:", contact, c.RESET)
     except:
-        print("Failed to Get Contact!")
+        print(f"{c.RED}Failed to Get Contact!{c.RESET}")
 
 
-    print("\nSTATUS: Get Place Schedules...")
+    print(f"{c.YELLOW}\nSTATUS: Get Place Schedules...{c.RESET}")
     try:
-        table_rows = WebDriverWait(driver, 5).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'table.eK4R0e tr'))
-        )
+        table_rows = querySelectorAll(driver, 'table.eK4R0e tr')
         for row in table_rows:
             e_day = querySelector(row, "td.ylH6lf div")
             e_hours = querySelector(row, "td.mxowUb li")
@@ -90,24 +122,22 @@ def scrap(url, img_amount = 0):
             
             if "Tutup" in hours: continue
 
-            time = hours.split('–')
+            time = hours.replace('.', ':').split('–')
             
-            schedules[day] = {
+            schedules[day_generalization[day]] = {
                 'time-start': time[0],
                 'time-end': time[1],
             }
 
-        print("FOUND:", schedules)
+        print(f"{c.GREEN}FOUND:", schedules, c.RESET)
     except:
-        print("Failed to Get Schedules!")
+        print(f"{c.RED}Failed to Get Schedules!{c.RESET}")
     
     
     try:
-        print("\nSTATUS: Get Place Pricelist...")
+        print(f"{c.YELLOW}\nSTATUS: Get Place Pricelist...{c.RESET}")
 
-        table_rows = WebDriverWait(driver, 5).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'table.rqRH4d tr'))
-        )
+        table_rows = querySelectorAll(driver, 'table.rqRH4d tr')
         all_polling = []
 
         for row in table_rows:
@@ -123,12 +153,12 @@ def scrap(url, img_amount = 0):
         price_min = int(most_polling[0]['pricelist'][0])
         price_max = int(most_polling[0]['pricelist'][1])
 
-        print("FOUND:", [price_min, price_max])
+        print(f"{c.GREEN}FOUND:", [price_min, price_max], c.RESET)
     except:
-        print("Failed to Get Pricelist!")
+        print(f"{c.RED}Failed to Get Pricelist!{c.RESET}")
 
 
-    print("\nSTATUS: Get Coordinate...")
+    print(f"{c.YELLOW}\nSTATUS: Get Coordinate...{c.RESET}")
     try:
         current_url = driver.current_url
         match = re.search(r"@(-?\d+(\.\d+)?),(-?\d+(\.\d+)?),(\d+)z", current_url)
@@ -139,12 +169,12 @@ def scrap(url, img_amount = 0):
         else:
             raise Exception()
         
-        print("FOUND:", [longitude, latitude])
+        print(f"{c.GREEN}FOUND:", [longitude, latitude], c.RESET)
     except:
-        print('Failed to Get Coordinate')
+        print(f'{c.RED}Failed to Get Coordinate{c.RESET}')
 
     if img_amount != 0:
-        print("\nSTATUS: Get Images Sources...")
+        print(f"{c.YELLOW}\nSTATUS: Get Images Sources...{c.RESET}")
 
         try:
             link = WebDriverWait(driver, 5).until(
@@ -152,24 +182,23 @@ def scrap(url, img_amount = 0):
             )
             link.click()
 
-            image_containers = WebDriverWait(driver, 5).until(
-                EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.m6QErb.DxyBCb.kA9KIf.dS8AEf.XiKgde > div > div"))
-            )
+            image_containers = querySelectorAll(driver, "div.m6QErb.DxyBCb.kA9KIf.dS8AEf.XiKgde > div > div")
 
             for i in range(img_amount):
-                driver.execute_script("document.querySelector('div.m6QErb.DxyBCb.kA9KIf.dS8AEf.XiKgde').scrollBy(0, window.innerHeight)")
-                sleep(0.2)
+                try:
+                    driver.execute_script("document.querySelector('div.m6QErb.DxyBCb.kA9KIf.dS8AEf.XiKgde').scrollBy(0, window.innerHeight)")
+                    sleep(0.2)
 
-                div = WebDriverWait(image_containers[i], 5).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, 'a.OKAoZd > div.U39Pmb > .Uf0tqf.loaded'))
-                )
-                src = div.get_attribute('style').strip().split('"')[1]
+                    div = querySelector(image_containers[i], 'a.OKAoZd > div.U39Pmb > div.Uf0tqf.loaded')
+                    src = div.get_attribute('style').strip().split('"')[1]
 
-                images.append(src)
+                    images.append(src)
+                except:
+                    print(f'{c.MAGENTA}Some source may not found! Try another...{c.RESET}')
             
-            print("FOUND:", images)
+            print(f"{c.GREEN}FOUND:", images, c.RESET)
         except:
-            print("Failed to Get Image Sources!")
+            print(f"{c.RED}Failed to Get Image Sources!{c.RESET}")
 
     return {
         "name": name,
@@ -186,6 +215,8 @@ def scrap(url, img_amount = 0):
     }
 
 def main():
+    c_init()
+
     global driver
 
     links = None
@@ -196,7 +227,7 @@ def main():
 
         for i in range(len(argvs)):
             if argvs[i] == '-h':
-                print("Usage: python scrapper.py [-L FILE] [-a IMG_AMOUNT]")
+                print(f"{c.YELLOW}Usage: python scrapper.py [-L FILE] [-a IMG_AMOUNT]{c.RESET}")
                 sys.exit(1)
             if argvs[i] == '-L':
                 i += 1
@@ -215,7 +246,7 @@ def main():
         
         driver = webdriver.Chrome()
 
-        print(f"\n=========== USING ============= \nURL    = {url} \nAmount = {img_amount}")
+        print(f"\n=========== USING ============= \n{c.CYAN}URL    = {url} \nAmount = {img_amount} {c.RESET}")
         print("========== SCRAPPING ==========")
 
         res = scrap(url, img_amount)
@@ -224,7 +255,7 @@ def main():
         driver = webdriver.Chrome()
 
         for url in links:
-            print(f"\n=========== Using ============ \nURL    = {url} \nAmount = {img_amount}")
+            print(f"\n=========== Using ============ \n{c.CYAN}URL    = {url} \nAmount = {img_amount} {c.RESET}")
             print("========== SCRAPPING ==========")
             
             res = scrap(url, img_amount)
@@ -235,7 +266,7 @@ def main():
     print("\n======== END SCRAPPING ========")
 
 
-    print("\nProcess Data & Download Images...")
+    print(f"{c.YELLOW}\nProcess Data & Download Images...{c.RESET}{c.GREEN}")
     os.makedirs('result', exist_ok=True)
 
     for item in tqdm(result):
